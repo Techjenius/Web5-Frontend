24e848f4d3310bba07068bd04e98c03dc5c495d1
Unclephil <philipnssien@gmail.com>
2023-11-24 00:19:31 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h3>
+          Hi there, Welcome to the TeamGenius Music Platform.
+        </h3>
+        <p>Powered by TBD(Web5).</p>
         <a
           className="App-link"
           href="https://reactjs.org"
